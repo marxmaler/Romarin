d7f62c0d019449cf63d036299e4aee3bdf2e838f
Sabi <84113149+marxmaler@users.noreply.github.com>
2022-07-18 14:19:12 +0900

    cors config test afterbuild

--- build/server/server.js
+++ build/server/server.js
@@ -8,6 +8,7 @@ require("./db");
 require("./models/Word");
 require("./models/User");
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const express_session_1 = __importDefault(require("express-session"));
 const connect_mongo_1 = __importDefault(require("connect-mongo"));
 const morgan_1 = __importDefault(require("morgan"));
@@ -16,12 +17,17 @@ const apiRouter_1 = __importDefault(require("./router/apiRouter"));
 const helmet_1 = __importDefault(require("helmet"));
 const app = (0, express_1.default)();
 const buildAddress = path_1.default.join(__dirname, "..", "..", "client/build/");
-console.log("current directory:", __dirname);
+// console.log("current directory:", __dirname);
+const corsOptions = {
+    origin: `http://15.164.98.24:${process.env.PORT}`,
+    credentials: true,
+};
 //전역 middleware 선언부
 app.use((0, helmet_1.default)());
 app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
+app.use((0, cors_1.default)(corsOptions));
 app.use((0, express_session_1.default)({
     secret: process.env.SESSION_SECRET + "",
     resave: false,
